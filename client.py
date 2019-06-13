abeb08d5d2972789a769b3f183e78328aaac6d6d
wf <mytemp2@163.com>
2019-06-13 15:28:14 +0800

    implement the logic for one client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+# from tkinter import messagebox
 import socket
 import sys
 import settings
@@ -7,6 +8,7 @@ HOST = settings.SERVER_HOST
 PORT = settings.SERVER_PORT
 CUR_WINDOW_NAME = settings.CLIENT_HOST[settings.WINDOW_IP]
 
+numbers = settings.NUMS
 
 class QMS_Client:
     def __init__(self, root):
@@ -19,14 +21,14 @@ class QMS_Client:
 
         frame_buttons = tk.Frame(root)
         miss = tk.Button(frame_buttons, text='过号',
-                         fg='red', command=self.miss_number)
+                         fg='red', command=self.click_miss_number)
         miss.pack(side='left', padx=10, pady=10)
         start = tk.Button(frame_buttons, text='开始办理',
-                          fg='black', command=self.start_number)
+                          fg='black', command=self.click_start_number)
         start.pack(side='left', padx=10, pady=10)
-        done = tk.Button(frame_buttons, text='完成', fg='green',
-                         command=self.finish_number)
-        done.pack(side='left', padx=10, pady=10)
+        finish = tk.Button(frame_buttons, text='完成', fg='green',
+                         command=self.click_finish_number)
+        finish.pack(side='left', padx=10, pady=10)
         frame_buttons.pack()
 
         frame_status = tk.Frame(root)
@@ -40,28 +42,49 @@ class QMS_Client:
         # root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def send_info(self, mode, num):
-        # mdoe: 'start', 'done', 'pause', 'miss'
+        # mdoe: 'start', 'finish', 'pause', 'miss'
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             # Connect to server and send data
             info = ','.join([str(CUR_WINDOW_NAME), str(mode), str(num)])
-            sock.connect((HOST, PORT))
+            try:
+                sock.connect((HOST, PORT))
+            except:
+                self.status['text'] = '无法连接到服务端！'
+                return
             sock.sendall(bytes(info + "\n", "utf-8"))
 
             # Receive data from the server and shut down
-            received = str(sock.recv(1024), "utf-8")
+            received = str(sock.recv(4096), "utf-8")
 
             print("Sent:     {}".format(info))
             print("Received: {}".format(received))
 
-    def is_valid_num(self, string_to_check):
-        if not string_to_check:
-            return False
-        for c in string_to_check:
-            if c not in '0123456789':
-                return False
-        return True
+            if mode in ['miss', 'finish']:
+                # if len(numbers) <= 1:
+                #     self.status['text'] = '暂无办理。'
+                # else:
+                if '|' in received:
+                    wait_num, numbers = received.split('|')
+                    numbers = numbers.split(',')
+                    self.cur_nubmer.insert(0, wait_num)
+                    self.status['text'] = '正在等待{}号...'.format(wait_num)
+                else:
+                    self.status['text'] = '暂无办理。'
 
-    def start_number(self):
+    def is_valid_num(self, string_to_check):
+        # if not string_to_check:
+        #     return False
+        # for c in string_to_check:
+        #     if c not in '0123456789':
+        #         return False
+        # return True
+        try:
+            str_to_int = int(string_to_check)
+            return str_to_int in settings.NUMS
+        except:
+            return False
+
+    def click_start_number(self):
         num = self.cur_nubmer.get().strip()
         if not self.is_valid_num(num):
             self.status['text'] = '输入号码无效！'
@@ -69,9 +92,9 @@ class QMS_Client:
             return
         # self.cur_nubmer.delete(0, tk.END)
         self.status['text'] = '正在为{}号办理业务...'.format(num)
-        self.send_info('start', 2)
+        self.send_info('start', num)
 
-    def miss_number(self):
+    def click_miss_number(self):
         num = self.cur_nubmer.get().strip()
         if not self.is_valid_num(num):
             self.status['text'] = '输入号码无效！'
@@ -79,8 +102,9 @@ class QMS_Client:
             return
         self.cur_nubmer.delete(0, tk.END)
         self.status['text'] = '{}号已过号！'.format(num)
+        self.send_info('miss', num)
 
-    def finish_number(self):
+    def click_finish_number(self):
         num = self.cur_nubmer.get().strip()
         if not self.is_valid_num(num):
             self.status['text'] = '输入号码无效！'
@@ -88,6 +112,7 @@ class QMS_Client:
             return
         self.cur_nubmer.delete(0, tk.END)
         self.status['text'] = '{}号已完成！'.format(num)
+        self.send_info('finish', num)
 
     # def on_closing(self):
     #     pass
@@ -97,3 +122,4 @@ root = tk.Tk()
 root.title('排号系统')
 app = QMS_Client(root)
 root.mainloop()
+
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import socketserver
 import threading
+from time import sleep
 
 import settings
 
@@ -9,20 +10,22 @@ PORT = settings.SERVER_PORT
 
 
 class QMS_Server:
+    """Server side, show current working status with info received from client side(s) and allocate customers."""
+
     def __init__(self, root):
         self.root = root
         frame1 = tk.Frame(self.root)
-        self.w1 = tk.Label(frame1, text=self.gen_text(1, 1))
-        self.w1.pack(side='top', padx=50, pady=5)
-        self.w2 = tk.Label(frame1, text=self.gen_text(2, 2))
+        self.w1 = tk.Label(frame1, text=self.gen_text(1))
+        self.w1.pack(side='top', pady=5)
+        self.w2 = tk.Label(frame1, text=self.gen_text(2))
         self.w2.pack(side='top', pady=5)
-        self.w3 = tk.Label(frame1, text=self.gen_text(3, 3))
+        self.w3 = tk.Label(frame1, text=self.gen_text(3))
         self.w3.pack(side='top', pady=5)
-        self.w4 = tk.Label(frame1, text=self.gen_text(4, 4, ''))
+        self.w4 = tk.Label(frame1, text=self.gen_text(4))
         self.w4.pack(side='top', pady=5)
-        # self.w5 = tk.Label(frame1, text=self.gen_text(5, 5, ''))
+        # self.w5 = tk.Label(frame1, text=self.gen_text(5))
         # self.w5.pack(side='top',pady=5)
-        frame1.pack()
+        frame1.pack(padx=50)
 
         # always on top
         self.root.attributes('-topmost', 'true')
@@ -47,8 +50,6 @@ class QMS_Server:
             # self.server.shutdown()
         # print('end_thread')
 
-
-
     # def listen_sock(self):
     #     self.s_sock.listen(5)
     #     while True:
@@ -57,11 +58,13 @@ class QMS_Server:
     #         c_sock.send('info send from s_sock')
     #         c_sock.close()
 
-    def gen_text(self, window_num, cur_num, type='doing'):
+    def gen_text(self, window_num, type='no_work', cur_num=0):
         if type == 'doing':
             text = '{}号窗口: 正在为{}号办理'.format(window_num, cur_num)
-        else:
+        elif type == 'waiting':
             text = '{}号窗口: 正在等待{}号...'.format(window_num, cur_num)
+        elif type == 'no_work':
+            text = '{}号窗口: 暂无办理。'.format(window_num)
         return text
 
     def on_closing(self):
@@ -71,26 +74,66 @@ class QMS_Server:
         self.socket_server_thread.join()
         # print(3)
         self.root.destroy()
-        
+
         # pass
         # self.s_sock.close()
 
 
 class MyTCPHandler(socketserver.StreamRequestHandler):
+    # def __init__(self, window_labels):
+    #     self.window_labels = window_labels
 
     def handle(self):
         # self.rfile is a file-like object created by the handler;
         # we can now use e.g. readline() instead of raw recv() calls
         self.data = self.rfile.readline().strip()
         print("{} wrote:".format(self.client_address[0]))
-        print(str(self.data, encoding='utf-8'))
+        recv_info = str(self.data, encoding='utf-8')
+        print(recv_info)
         # Likewise, self.wfile is a file-like object used to write back
-        # to the client
-        self.wfile.write(self.data.upper())
+
+        window_idx = settings.CLIENT_ORDER.index(self.client_address[0])
+        mode, polished_info, win_num, customer_num = self.info_parser(
+            recv_info)
+        labels[window_idx]['text'] = polished_info
+        if mode in ['miss', 'finish']:
+            # print('here!')
+            # sleep(.5)
+            if len(numbers) <= 1:
+                labels[window_idx]['text'] = '{}：暂无办理。'.format(win_num)
+                self.wfile.write(bytes('', 'utf-8'))
+            else:
+                numbers.remove(int(customer_num))
+                next_num = numbers[0]
+                # next_num = numbers.pop(0)
+                labels[window_idx]['text'] = '{}：正在等待{}号办理...'.format(
+                    win_num, next_num)
+                print(numbers)
+                # to the client
+                byte_numbers = bytes(str(next_num) + '|' + ','.join([str(n) for n in numbers]), 'utf-8')
+                self.wfile.write(byte_numbers)
+        elif mode == 'start':
+            self.wfile.write(bytes('', 'utf-8'))
+
+    def info_parser(self, info):
+        win_num, mode, customer_num = info.split(',')
+        polished = ''
+        if mode == 'start':
+            polished = '{}：正在为{}号办理'.format(win_num, customer_num)
+        elif mode == 'miss':
+            polished = '{}：{}号已过号！'.format(win_num, customer_num)
+        elif mode == 'finish':
+            polished = '{}：{}号已完成。'.format(win_num, customer_num)
+        return mode, polished, win_num, customer_num
 
 
 root = tk.Tk()
 root.title('办理进度')
 server = QMS_Server(root)
+
+labels = [server.w1, server.w2, server.w3, server.w4]
+numbers = settings.NUMS
+
+# print(numbers)
+
 root.mainloop()
-# server.listen_sock()
